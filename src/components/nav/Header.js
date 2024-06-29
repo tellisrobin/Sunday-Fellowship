187aa7d21a065b45a52da06d4b4838cc11acbe38
BITLA AMRUTH.RAJ <98109005+BITLAAMRUTHRAJ@users.noreply.github.com>
2024-06-28 20:33:57 -0400

    Update Header.js

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { NavLink } from 'react-router-dom';
 import { Col, Row, Drawer } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
-import './Main.css'; // Import the CSS file
+import './Main.css';
 
-function Main() {
+const Header = () => {
   const [drawerVisible, setDrawerVisible] = useState(false);
 
   const showDrawer = () => {
@@ -21,43 +21,43 @@ function Main() {
     <>
       <Row align="middle" justify="space-between" className="header">
         <Col xs={18} sm={8} md={6} lg={4}>
-          <NavLink className="nav-link" activeClassName="active" exact to="/">
+          <NavLink className="nav-link ripple" exact to="/" aria-current="page">
             <p className="logo">Logo</p>
           </NavLink>
         </Col>
         <Col xs={6} sm={0} md={0} lg={0} className="drawer-icon-container">
-          <p className="sidebar" onClick={showDrawer}>
+          <p className="sidebar ripple" onClick={showDrawer}>
             <MenuOutlined />
           </p>
         </Col>
         <Col xs={0} sm={16} md={14} lg={12}>
           <div className="nav-links-container">
-            <NavLink className="nav-link" activeClassName="active" exact to="/team">
+            <NavLink className="nav-link ripple" exact to="/team" aria-current="page">
               Our Team
             </NavLink>
-            <NavLink className="nav-link" activeClassName="active" exact to="/who-we-are">
+            <NavLink className="nav-link ripple" exact to="/who-we-are" aria-current="page">
               Who We Are
             </NavLink>
-            <NavLink className="nav-link" activeClassName="active" exact to="/events">
+            <NavLink className="nav-link ripple" exact to="/events" aria-current="page">
               Events
             </NavLink>
-            <NavLink className="nav-link" activeClassName="active" exact to="/contact-us">
+            <NavLink className="nav-link ripple" exact to="/contact-us" aria-current="page">
               Contact us
             </NavLink>
           </div>
         </Col>
       </Row>
       <Drawer title="Menu" placement="right" onClose={onClose} visible={drawerVisible}>
-        <NavLink className="nav-link" activeClassName="active" exact to="/team" onClick={onClose}>
+        <NavLink className="nav-link ripple" exact to="/team" onClick={onClose} aria-current="page">
           Our Team
         </NavLink>
-        <NavLink className="nav-link" activeClassName="active" exact to="/who-we-are" onClick={onClose}>
+        <NavLink className="nav-link ripple" exact to="/who-we-are" onClick={onClose} aria-current="page">
           Who We Are
         </NavLink>
-        <NavLink className="nav-link" activeClassName="active" exact to="/events" onClick={onClose}>
+        <NavLink className="nav-link ripple" exact to="/events" onClick={onClose} aria-current="page">
           Events
         </NavLink>
-        <NavLink className="nav-link" activeClassName="active" exact to="/contact-us" onClick={onClose}>
+        <NavLink className="nav-link ripple" exact to="/contact-us" onClick={onClose} aria-current="page">
           Contact us
         </NavLink>
       </Drawer>
@@ -65,4 +65,4 @@ function Main() {
   );
 }
 
-export default Main;
+export default Header;
